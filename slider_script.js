dfc51d491113539ddd30bea0ea678ca2cf0dc8ae
lwandoM <54123191+lwandoM@users.noreply.github.com>
2019-10-09 20:22:49 +0200

    Initial commit
    Commit 1

---
+++ slider_script.js
@@ -0,0 +1,43 @@
+var Index = 1;
+showSlides(Index);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(Index += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(Index = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {Index = 1}
+  if (n < 1) {Index = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[Index-1].style.display = "block";
+  dots[Index-1].className += " active";
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
